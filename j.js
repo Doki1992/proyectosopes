a30d7a74b1a9ab9acefeac8b38d07a25e3b7c01d
mike <mike58072@gmail.com>
2018-05-21 07:46:30 -0600

    d

--- j.js
+++ j.js
@@ -1,3 +1,20 @@
 
 
-$('.c1').html('mike');
+
+
+setTimeout(callback, 200);
+
+function callback()
+{
+	$.ajax(
+			url: "worker.php",
+			method: POST,
+			success: function(response){
+				alert(response);
+			},
+			error: function(error){
+				alert(error.responseText);
+			}
+		);
+
+}
