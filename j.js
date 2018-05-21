0820bb2f2f47c8fa44a73e063c45e16221f17716
mike <mike58072@gmail.com>
2018-05-21 08:29:06 -0600

    d

--- j.js
+++ j.js
@@ -7,14 +7,24 @@ setInterval(callback, 500);
 function callback()
 {
 	$.ajax({
-			url: "worker.php",
-			method: 'POST',
-			success: function(response){
-				alert(response);
-			},
-			error: function(error){
-				alert(error.responseText);
-			}
+    // En data puedes utilizar un objeto JSON, un array o un query string
+    data: {"parametro1" : "valor1", "parametro2" : "valor2"},
+    //Cambiar a type: POST si necesario
+    type: "GET",
+    // Formato de datos que se espera en la respuesta
+    dataType: "json",
+    // URL a la que se enviará la solicitud Ajax
+    url: "worker.php",
+})
+ .done(function( data, textStatus, jqXHR ) {
+     if ( console && console.log ) {
+         console.log( "La solicitud se ha completado correctamente." );
+     }
+ })
+ .fail(function( jqXHR, textStatus, errorThrown ) {
+     if ( console && console.log ) {
+         console.log( "La solicitud a fallado: " +  textStatus);
+     }
 });
 }
 
