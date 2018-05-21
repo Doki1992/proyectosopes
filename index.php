2e7ce276a0e0dcd097662592e6396984beed0ab4
doki1992 <mike58072@gmail.com>
2018-05-21 00:42:10 +0000

    mongo

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$valu = "cola" + rand(1,3);
+define("RABBITMQ_HOST", '35.196.227.205');
+define("RABBITMQ_PORT", 5672);
+define("RABBITMQ_USERNAME", "guest");
+define("RABBITMQ_PASSWORD", "guest");
+define("RABBITMQ_QUEUE_NAME", $valu);
+
+$connection = new \PhpAmqpLib\Connection\AMQPStreamConnection(
+    RABBITMQ_HOST, 
+    RABBITMQ_PORT, 
+    RABBITMQ_USERNAME, 
+    RABBITMQ_PASSWORD
+);
+
+
+$channel = $connection->channel();
+# Create the queue if it doesnt already exist.
+$channel->queue_declare(
+    $queue = RABBITMQ_QUEUE_NAME,
+    $passive = false,
+    $durable = true,
+    $exclusive = false,
+    $auto_delete = false,
+    $nowait = false,
+    $arguments = null,
+    $ticket = null
+);
+
+
+$callback = function($msg){
+    $job = json_decode($msg->body, $assocForm=true);
+    sleep(1);
+#$m = new MongoClient();
+#$bd = $m->practica;
+#$colección = $bd->pr;
+#$colección->insert([ 'user' => $job['user'], 'mensaje' => $job['mensaje'], 'tipo' => $job['tipo'] );
+
+#$cliente = new MongoDB\Client("mongodb://localhost:27017", [
+#	'username' => 'mike',
+#	'password' => 'mike',
+#	'authSource' => 'admin',	
+#	'db' => 'admin'
+#]);
+#$colección = $cliente->admin->beers;
+#$resultado = $colección->insertOne([ 'user' => $job['user'], 'mensaje' => $job['mensaje'], 'tipo' => $job['tipo']] );
+
+   # echo " [x] Done", "\n";
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+	echo($job);
+};
+
+$channel->basic_qos(null, 1, null);
+
+$channel->basic_consume(
+    $queue = RABBITMQ_QUEUE_NAME,
+    $consumer_tag = '',
+    $no_local = false,
+    $no_ack = false,
+    $exclusive = false,
+    $nowait = false,
+    $callback
+);
+
+while (count($channel->callbacks)) 
+{
+	    
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
