944694ed4788450a2e0ca354bd27837c8b033413
mike <mike58072@gmail.com>
2018-05-20 22:40:52 -0600

    e

--- prueba.php
+++ prueba.php
@@ -20,7 +20,7 @@ $labels = $response->getLabelAnnotations();
 
 if ($labels) {
     echo("Labels:" . PHP_EOL);
-    echo(json_decode($labels) .PHP_EOL);
+    echo(json_encode($labels) .PHP_EOL);
     foreach ($labels as $label) {
         echo($label->getDescription() . PHP_EOL);
     }
