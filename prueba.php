02b57c1c56e11530ac0e5c767d72738b5e045008
mike <mike58072@gmail.com>
2018-05-20 22:37:51 -0600

    e

--- prueba.php
+++ prueba.php
@@ -9,7 +9,7 @@ use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 $imageAnnotator = new ImageAnnotatorClient();
 
 # the name of the image file to annotate
-$fileName = 'https://cf-cdn.gananci.com/wp-content/uploads/2017/05/op.jpg';
+$fileName = 'https://cf-cdn.gananci.com/wp-content/uploads/2017/05/felicidad-619x346.jpg';
 
 # prepare the image to be annotated
 $image = file_get_contents($fileName);
