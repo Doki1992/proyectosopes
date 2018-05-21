2df62db2e123e30a7d4b34f01445b1177ea3ca6a
mike <mike58072@gmail.com>
2018-05-21 07:54:50 -0600

    d

--- j.js
+++ j.js
@@ -6,7 +6,10 @@ setTimeout(callback, 200);
 
 function callback()
 {
-	$.ajax({
+	alert('200');
+}
+
+$.ajax({
 			url: "worker.php",
 			method: 'POST',
 			success: function(response){
@@ -15,6 +18,4 @@ function callback()
 			error: function(error){
 				alert(error.responseText);
 			}
-		});
-
-}
+});
